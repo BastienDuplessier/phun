36f8d872cf2525d06be2599308a4b4071e70b0a0
xvw <xavier.vdw@gmail.com>
2015-06-20 16:43:12 +0200

    Append Ugly Syntax

--- index.php
+++ index.php
@@ -11,51 +11,42 @@ $hello_quelqun = new Service(
 
 use Html5 as D;
 
-$hello->bindWith(
-    function($get, $post) use ($hello_quelqun) {
-        echo D\html(
+function page($body) {
+    echo
+        D\html(
             [
                 D\head([
                     D\title([], ["Test"]),
                     D\meta(["charset" => "utf-8"], []),
                 ]),
-                D\body([], [
-                    D\h1([], ["Salut le monde!"]),
-                    $hello_quelqun->link(
-                        ["Dire coucou à Xavier"],
-                        ["prenom" => "xavier"],
-                        ["style" => "background-color:green;
+                D\body([], $body)
+            ]);
+}
+
+
+$hello->bindWith(
+    function($get, $post) use ($hello_quelqun) {
+        page([ D\h1([], ["Salut le monde!"]),
+               $hello_quelqun->link(
+                   ["Dire coucou à Xavier"],
+                    ["prenom" => "xavier"],
+                   ["style" => "background-color:green;
                           color:white;
                           padding:8px;"
-                        ] 
-                    )
-                ])
-            ]
-        );
+                   ])]);
     }
 );
 
 $hello_quelqun->bindWith(
     function($get, $post) use ($hello) {
-        echo D\html(
-            [
-                D\head([
-                    D\title([], ["Test"]),
-                    D\meta(["charset" => "utf-8"], []),
-                ]),
-                D\body([], [
-                    D\h1([], ["Salut ". $get['prenom']]),
-                    $hello->link(
-                        ["Dire coucou au monde"],
-                        [],
-                        ["style" => "background-color:red;
+        page([ D\h1([], ["Salut ".$get['prenom']]),
+               $hello->link(
+                   ["Dire coucou au monde"],
+                   [],
+                   ["style" => "background-color:red;
                           color:white;
                           padding:8px;"
-                        ] 
-                    )
-                ])
-            ]
-        );
+                   ])]);
     }
 );
 
