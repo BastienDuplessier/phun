23e150e2839f2386864976b530dab67693af37b2
xvw <xavier.vdw@gmail.com>
2015-06-20 07:51:27 +0200

    Publish little sample

--- index.php
+++ index.php
@@ -3,7 +3,26 @@ require 'configuration.php';
 require 'core/phun.php';
 
 
-// Here place your application
+// Home service
+$hello = new Service(['hello']);
+$hello_xavier = new Service(
+    ['hello'], [Parameter::get('to', 'string', true)]
+);
+
+// Services linking
+$hello->bindWith(function($get, $post) use($hello_xavier) {
+    echo '<h1>Hello World!</h1>';
+    echo $hello_xavier->link(
+        'Dire bonjour à PHUN',
+        ['to' => 'PHUN']
+    );
+});
+
+$hello_xavier->bindWith(function($get, $post) use ($hello) {
+    echo '<h1>Hello '.$get['to'].'!</h1>';
+    echo $hello->link('Dire bonjour au monde');
+});
+
 
 
 Phun::start();
