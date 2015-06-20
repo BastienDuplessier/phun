8739291aa2e3784c45d4315b09944b1bac7a8b95
xvw <xavier.vdw@gmail.com>
2015-06-20 17:10:32 +0200

    restore old syntax

--- index.php
+++ index.php
@@ -1,57 +1,36 @@
 <?php
-
 require 'configuration.php';
 require 'core/phun.php';
 
-$hello = new \Service();
-$hello_quelqun = new \Service(
+$hello = new Service();
+$hello_quelqun = new Service(
     ["hello"],
-    [\Parameter::get('prenom', 'string', true)]
+    [Parameter::get('prenom', 'string', true)]
 );
-
-use Html5 as t;
-
-function page($body) {
-    echo
-        t\html(
-            [
-                t\head([
-                    t\title([], ["Test"]),
-                    t\meta(["charset" => "utf-8"], []),
-                ]),
-                t\body([], $body)
-            ]);
-}
-
 
 $hello->bindWith(
     function($get, $post) use ($hello_quelqun) {
-        page([ t\h1([], ["Salut le monde!"]),
-               $hello_quelqun->link(
-                   ["Dire coucou à Xavier"],
-                    ["prenom" => "xavier"],
-                   ["style" => "background-color:green;
-                          color:white;
-                          padding:8px;"
-                   ])]);
+        echo '<h1>Hello les gens (et le world)!</h1>';
+        echo $hello_quelqun->link(
+            'Dire coucou à Xavier!',
+            ['prenom' => 'Xavier'],
+            ["style" => "background-color:red;color:white;padding:8px;"] 
+        );
     }
 );
 
 $hello_quelqun->bindWith(
     function($get, $post) use ($hello) {
-        page([ t\h1([], ["Salut ".$get['prenom']]),
-               $hello->link(
-                   ["Dire coucou au monde"],
-                   [],
-                   ["style" => "background-color:red;
-                          color:white;
-                          padding:8px;"
-                   ])]);
+       echo '<h1>Hello '.$get['prenom'].'!</h1>';
+        echo $hello->link(
+            'Dire coucou au monde!',
+            [],
+            ["style" => "background-color:green;color:white;padding:8px;"] 
+        );
     }
 );
 
 
 
-
-\Phun::start();
+Phun::start();
 ?>
